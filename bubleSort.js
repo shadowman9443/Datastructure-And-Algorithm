631494a35fa7806af82bf6d1f0a4c3fb6991dbc6
shadowman9443 <shadowman9443@gmail.com>
2018-11-13 09:38:46 +0600

    from ofc selection sort , bubble sort

---
+++ bubleSort.js
@@ -0,0 +1,18 @@
+function bubleSort(arr) {
+    let noSwap;
+    for (let i = arr.length; i > 0; i--) {
+        noSwap = true;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                noSwap = false;
+            }
+
+        }
+        if (noSwap) break;
+    }
+    return arr;
+}
+console.log(bubleSort([9, 1, 11, 15, 19]));
