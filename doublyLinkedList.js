44177e3b5468c4150784a6eb7d39b784fad492ce
aunogarafat <shadowman9443@gmail.com>
2018-12-28 10:58:51 +0600

    complete the get

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,76 +1,100 @@
 class Node {
-    constructor(val){
+    constructor(val) {
         this.val = val;
         this.next = null;
         this.prev = null;
     }
 }
-class DoublyLinkedList{
-    constructor(){
+class DoublyLinkedList {
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
-    push(val){
+    push(val) {
         let newNode = new Node(val);
-        if(this.length === 0 ){
+        if (this.length === 0) {
             this.head = newNode;
             this.tail = newNode;
 
-        }else{
-            this.tail.next =newNode;
+        } else {
+            this.tail.next = newNode;
             newNode.prev = this.tail;
             this.tail = newNode;
         }
-        this.length ++;
+        this.length++;
         return this;
     }
-    pop(){
-        if(!this.head){
+    pop() {
+        if (!this.head) {
             return undefined;
         }
         let temp = this.tail;
-        if(this.length ===1){
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
-        }else{
-            this.tail = temp.prev ;
-            this.tail.next  = null;
-            temp.prev  = null;
+        } else {
+            this.tail = temp.prev;
+            this.tail.next = null;
+            temp.prev = null;
         }
-        this.length --;
+        this.length--;
         return this;
-      
+
     }
-    shift(){
-        if(!this.head){
+    shift() {
+        if (!this.head) {
             return undefined;
         }
         let temp = this.head;
-        if(this.length ===1){
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
-        }else{
-            this.head = temp.next ;
-            this.head.prev  = null;
-            temp.next  = null;
+        } else {
+            this.head = temp.next;
+            this.head.prev = null;
+            temp.next = null;
         }
-        this.length --;
+        this.length--;
         return this;
     }
-    punshift(val){
+    unshift(val) {
         let newNode = new Node(val);
-        if(this.length === 0 ){
+        if (this.length === 0) {
             this.head = newNode;
             this.tail = newNode;
 
-        }else{
-            newNode.next =  this.head;
+        } else {
+            newNode.next = this.head;
             this.head.prev = newNode;
             this.head = newNode;
         }
-        this.length ++;
+        this.length++;
         return this;
+    }
+    get(index) {
+        if (index < 0 || index >= this.length) {
+            return undefined;
+        }
+        let count = 0;
+        let current = null;
+        if (index <= this.length / 2) {
+            count = 0;
+            current = this.head;
+            while (count != index) {
+                current = current.next;
+                count++;
+            }
+
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while (count != index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
     }
 }
 
@@ -80,6 +104,5 @@ list.push(2);
 list.push(3);
 list.push(4);
 console.log(list);
-list.shift()
 console.log('**********');
-console.log(list);
+console.log(list.get(2));
