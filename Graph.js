1efc83ab95ce873fb98cded188a48ccde8eb14c7
aunogarafat <shadowman9443@gmail.com>
2019-01-22 21:16:23 +0600

    dfs iterative and dfs recursive

--- Graph.js
+++ Graph.js
@@ -18,11 +18,47 @@ class Graph{
         )
     }
     removeVertex(vertex){
-        while(his.adjaccencyList[vertextOne].length){
+        while(this.adjaccencyList[vertextOne].length){
             const adjaccentVertx = this.adjaccencyList(vertex).pop();
             this.removeEdge(vertex,adjaccentVertx);
         }
         delete this.adjaccencyList[vertex];
 
+    }
+
+    depthFirstRecursive(start){
+        const result = [];
+        const visited = {};
+        const adjaccencyList = this.adjaccencyList;
+        (function dfs(vertex){
+            if (!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjaccencyList[vertext].forEach(neighbour => {
+                if(!visited[neighbour]){
+                    return dfs(neighbour);
+                }
+            });
+        })(start)
+        return result;
+    }
+    depthFirstIterative(start){
+        const result = [];
+        const stack =[];
+        const visited = {};
+        stack.push(start);
+        visited[start] = true;
+        let i = stack.length;
+        let currentVertext ;
+        while(istack.length){
+            currentVertext =stack.pop();
+            result.push(currentVertext);
+            this.adjaccencyList[currentVertext].forEach(neighbour => {
+                if(!visited[neighbour]){
+                    stack.push(start);
+                }
+            });
+        }
+        return result; 
     }
 }
